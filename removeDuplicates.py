2a7a092cc048bac8d54d8069f34ff7e56bc933eb
Sinaf <166249512+sinaf777@users.noreply.github.com>
2025-04-25 23:44:14 +0300

    removeDuplicates.py

---
+++ removeDuplicates.py
@@ -0,0 +1,12 @@
+def removeDuplicates(nums: list[int]) -> int:
+    if not nums:
+        return 0
+
+    unique_index = 1
+
+    for i in range(1, len(nums)):
+        if nums[i] != nums[i - 1]:
+            nums[unique_index] = nums[i]
+            unique_index += 1
+    
+    return unique_index
