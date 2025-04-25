0e715a0458df4e946b6ae08fed90053901454583
Sinaf <166249512+sinaf777@users.noreply.github.com>
2025-04-25 23:41:22 +0300

    twoSum.py

---
+++ twoSum.py
@@ -0,0 +1,7 @@
+def twoSum(nums: list[int], target: int) -> list[int]:
+    num_map = {}  # value -> index
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_map:
+            return [num_map[complement], i]
+        num_map[num] = i
