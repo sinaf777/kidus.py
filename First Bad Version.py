e18f74ecbf4492f50a7876a73e1823b13bc0978a
Sinaf <166249512+sinaf777@users.noreply.github.com>
2025-04-25 23:48:11 +0300

    First Bad Version.py

---
+++ First Bad Version.py
@@ -0,0 +1,11 @@
+def firstBadVersion(n: int) -> int:
+    left, right = 1, n
+    
+    while left < right:
+        mid = left + (right - left) // 2
+        if isBadVersion(mid):
+            right = mid
+        else:
+            left = mid + 1
+    
+    return left
